9dc6dc2833e4ecaebe67842910a9af9f8277153c
Mitchell Wears II <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2024-01-01 01:28:25 -0500

    Update hook.cpp

--- hooks/hook.cpp
+++ hooks/hook.cpp
@@ -2,8 +2,8 @@
 #define clientdll __declspec(dllimport) void* __stdcall LoadLibrary("client.dll")
 #define engine2dll __declspec(dllimport) void* __stdcall LoadLibrary("engine2.dll")
 #elif __GNUC__
-#define clientdylib __attribute__(visibility) void* __stdcall dlopen("client.dynalib")
-#define engine2dynalib __attribute__(visibility) void* __stdcall dlopen("engine2.dynalib")
+#define clientdylib __attribute__(visibility) void* __stdcall dlopen("client.dylib")
+#define engine2dylib __attribute__(visibility) void* __stdcall dlopen("engine2.dylib")
 #endif
 
 #include <string.h>
