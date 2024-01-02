ad5f9afaf23f7ceacc5a41576e74d285eec80cfc
Mitchell Wears II <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2024-01-01 22:53:23 -0500

    Add files via upload

--- tf2_sdk/window.cpp
+++ tf2_sdk/window.cpp
@@ -1,1 +1,5 @@
-
+#include <SDL.h>
+class window {
+public:
+SDL_Window* window = SDL_CreateWindow("Team Fortress 2 Workshop Tools", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+};
